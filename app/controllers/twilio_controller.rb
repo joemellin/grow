10e91631fc0f49f3b04c61a19de46b131f7cac29
Joe Mellin <mellin.joe@gmail.com>
2014-09-18 18:43:17 -0700

    create conference

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -7,12 +7,26 @@ class TwilioController < ApplicationController
 		account_sid = 'AC8cc36fc273d176324fd6e2526c24b104'
 		auth_token = '75a8952da02e44984f2f340f21c29cb8'
 		client = Twilio::REST::Client.new account_sid, auth_token
-		call = client.account.calls.create(
+		call_to = client.account.calls.create(
 		:to => '+14152598215',
+		:from => '+13106969558',
+		:url => 'http://growapp.herokuapp.com/conference'
+		)
+		call_from = client.account.calls.create(
+		:to => '+13106969558',
 		:from => '+14152598215',
-		:url => 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.rock'
+		:url => 'http://growapp.herokuapp.com/conference'
 		)
 		redirect_to root_path
+	end
+
+	def conference
+		 response = Twilio::TwiML::Response.new do |r|
+      r.Dial 
+        r.Conference 'EBT Community Connection'
+    end
+ 
+    render_twiml response
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 	get '/signout' => 'sessions#destroy', :as => :signout
 	get '/auth/failure' => 'sessions#failure'
 	get 'twilio/call' => 'twilio#call'
-	
+	get 'conference' => 'twilio#conference'
 end
