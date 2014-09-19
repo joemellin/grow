7579661161720829fc003b217ef3148132cbf7ac
Joe Mellin <mellin.joe@gmail.com>
2014-09-18 19:21:02 -0700

    boom x2

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -6,7 +6,7 @@ class TwilioController < ApplicationController
 	# Get your Account Sid and Auth Token from twilio.com/user/account
 	skip_before_action :verify_authenticity_token
 
-
+	 
 	def call
 		account_sid = 'AC8cc36fc273d176324fd6e2526c24b104'
 		auth_token = '75a8952da02e44984f2f340f21c29cb8'
@@ -16,16 +16,20 @@ class TwilioController < ApplicationController
 		:from => '+14152598215',
 		:url => 'http://growapp.herokuapp.com/twilio/conference'
 		)
+		call_from = client.account.calls.create(
+		:to => '+13106969558',
+		:from => '+14152598215',
+		:url => 'http://growapp.herokuapp.com/twilio/conference'
+		)
 		redirect_to root_path
 	end
 
 	def conference
-		response = Twilio::TwiML::Response.new do |r|
+		 response = Twilio::TwiML::Response.new do |r|
 			r.Dial do |d|
-				d.Number '+13106969558'
-			end
+				d.Conference 'BOOM'
 		end
-
+ 
 		render_twiml response
 	end
 
