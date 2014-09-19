898ecb5908ecddf866c746147f892305a5325f76
Joe Mellin <mellin.joe@gmail.com>
2014-09-18 19:01:37 -0700

    keep doing it

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,7 +1,11 @@
+require 'twilio-ruby'
+
 class TwilioController < ApplicationController
-	 
+	include Webhookable
+	
 	# Get your Account Sid and Auth Token from twilio.com/user/account
 	skip_before_action :verify_authenticity_token
+
 	 
 	def call
 		account_sid = 'AC8cc36fc273d176324fd6e2526c24b104'
@@ -10,7 +14,7 @@ class TwilioController < ApplicationController
 		call_to = client.account.calls.create(
 		:to => '+14152598215',
 		:from => '+14152598215',
-		:url => 'http://growapp.herokuapp.com/conference'
+		:url => 'http://growapp.herokuapp.com/twilio/conference'
 		)
 		call_from = client.account.calls.create(
 		:to => '+13106969558',
