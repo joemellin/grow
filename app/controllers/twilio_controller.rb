22c51eb5fae5629f2887fab92cb03e6ff364d32e
Joe Mellin <mellin.joe@gmail.com>
2014-09-22 20:12:20 -0700

    changing welcoming prompt calling joe

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -45,7 +45,7 @@ class TwilioController < ApplicationController
 
 	def welcome_that_doesnt_call
 		response = Twilio::TwiML::Response.new do |r|
-			r.Say current_user.nick
+			r.Say "Welcoming #{current_user.nick}"
 			r.Dial do |d|
 				d.Conference 'Connecting'
 			end
