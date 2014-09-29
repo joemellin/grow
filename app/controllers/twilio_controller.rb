7873cd1bd779c0bc9c6a44867b03e7cc6a8eda2a
Joe Mellin <mellin.joe@gmail.com>
2014-09-29 11:30:14 -0700

    add first

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -112,10 +112,11 @@ class TwilioController < ApplicationController
 		response = Twilio::TwiML::Response.new do |r|
 			if User.where(:phone => params['From']).present? 
 				user = User.where(:phone => params['From'])
-				r.Say "Hi, #{user.nick} you have dialed the feel community line.  When feel members call you, you will receive the calls from this number.  To connect with a member visit the site and click connect. ", :voice => 'alice'
+				phone = user.first.nick
+				r.Say "Hi, #{phone} you have dialed the feel community line.  When feel members call you, you will receive the calls from this number.  To connect with a member visit the site and click connect. ", :voice => 'alice'
 					r.Play 'http://linode.rabasa.com/cantina.mp3'
 			else
-				
+
 			end
 		end
  
