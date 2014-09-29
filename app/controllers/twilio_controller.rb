eee7ca21b7da08cea8424a6db79bbe8a2b95c143
Joe Mellin <mellin.joe@gmail.com>
2014-09-29 10:37:27 -0700

    changed to double quotes

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -111,7 +111,7 @@ class TwilioController < ApplicationController
 	def voice_community
 		response = Twilio::TwiML::Response.new do |r|
 			phone = params['From'] || 'Monkey'
-			r.Say 'Hi, #{phone} you have dialed the feel community line.  When feel members call you, you will receive the calls from this number.  To connect with a member visit the site and click connect. ', :voice => 'alice'
+			r.Say "Hi, #{phone} you have dialed the feel community line.  When feel members call you, you will receive the calls from this number.  To connect with a member visit the site and click connect. ", :voice => 'alice'
 				r.Play 'http://linode.rabasa.com/cantina.mp3'
 		end
  
