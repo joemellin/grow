d53c2693b8cf6bb2100102e621b8c849e2fd2828
Joe Mellin <mellin.joe@gmail.com>
2014-09-29 14:33:14 -0700

    update spacing

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -114,8 +114,7 @@ class TwilioController < ApplicationController
 				user = User.where(:phone => params['From'])
 				phone = user.first.nick
 				r.Say "Hi, #{phone} this is the feel community line. Open the", :voice => 'alice'
-				r.Dial
-					r.Number "+14152598215"
+				r.Dial :number => "+14152598215"
 			else
 				r.Say "Hi you have received a call from the Feel community line.  To make a call visit feel by ebt dot com"
 			end
