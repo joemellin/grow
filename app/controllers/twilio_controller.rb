9b3d2934170c30073113e6fe449407f38fd3971d
Joe Mellin <mellin.joe@gmail.com>
2014-09-29 09:56:36 -0700

    remove get

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -110,18 +110,17 @@ class TwilioController < ApplicationController
 
 	def voice_community
 
-		get '/hello-monkey' do
-			people = {
-				'+16617480240' => 'Curious George',
-				'+14158675310' => 'Boots',
-				'+14158675311' => 'Virgil',
-				'+14158675312' => 'Marcel',
-			}
-			name = people[params['From']] || 'Monkey'
-			Twilio::TwiML::Response.new do |r|
-				r.Say "Hello #{name}"
-			end.text
-		end
+		people = {
+			'+16617480240' => 'Curious George',
+			'+14158675310' => 'Boots',
+			'+14158675311' => 'Virgil',
+			'+14158675312' => 'Marcel',
+		}
+		name = people[params['From']] || 'Monkey'
+		Twilio::TwiML::Response.new do |r|
+			r.Say "Hello #{name}"
+		end.text
+
 		
  
 		render_twiml response
