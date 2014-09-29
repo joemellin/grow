eff05b69a5e11a45bb420fc1804dac43f9deea15
Joe Mellin <mellin.joe@gmail.com>
2014-09-29 14:35:21 -0700

    new spacing

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -114,7 +114,10 @@ class TwilioController < ApplicationController
 				user = User.where(:phone => params['From'])
 				phone = user.first.nick
 				r.Say "Hi, #{phone} this is the feel community line. Open the", :voice => 'alice'
-				r.Dial :number => "+14152598215"
+				r.Dial :timeout => 30  do |d|
+					d.Number '+14152598215'
+				end
+			end 
 			else
 				r.Say "Hi you have received a call from the Feel community line.  To make a call visit feel by ebt dot com"
 			end
